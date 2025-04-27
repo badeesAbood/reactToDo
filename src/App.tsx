6c9fed36a7b6ff5ec6c9a2ae2f938a70c48881bc
badees abood <57720463+badees99@users.noreply.github.com>
2025-04-27 12:42:20 +0200

    add functionality , edit , and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,55 @@
-
-import './index.css'
-import TodoInput from './components/TodoInput'
-import TodoList from './components/TodoList'
-import { useState } from 'react'
+import "./index.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import { useEffect, useState } from "react";
 
 function App() {
-
-  const [todos, setTodos] =  useState<string[]>([])
+  const [todos, setTodos] = useState<string[]>([]);
 
   function handleAddTodo(todo: string) {
+    if (todo === "") {
+      alert("Please enter a todo");
+      return;
+    }
+    const newTodos = [...todos, todo];
 
-    const newTodos = [...todos, todo] 
-
-    console.log(newTodos)
-    setTodos(newTodos)
+    handleSaveTodos(newTodos);
+    setTodos(newTodos);
   }
 
+  // this two function should be handled by index but either way is fine for me
   function handleDeleteTodo(todo: string) {
-    const newTodos = todos.filter((t) => t !== todo)
-    setTodos(newTodos)
+    const newTodos = todos.filter((t) => t !== todo);
+    handleSaveTodos(newTodos);
+    setTodos(newTodos);
+  }
+  function handleEditTodo(oldTodo: string, newTodo: string) {
+    const newTodos = todos.map((t) => (t === oldTodo ? newTodo : t));
+    handleSaveTodos(newTodos);
+    setTodos(newTodos);
   }
 
-  function handleEditTodo(oldTodo: string, newTodo: string) {
-    const newTodos = todos.map((t) => (t === oldTodo ? newTodo : t))
-    setTodos(newTodos)
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  function handleSaveTodos(newTodos : string[]) {
+    localStorage.setItem("todos", JSON.stringify(newTodos));
   }
+
   return (
     <>
-      <TodoInput handleAddTodo={handleAddTodo}/> 
-      <TodoList todos={todos} handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} />
+      <TodoInput handleAddTodo={handleAddTodo} />
+      <TodoList
+        todos={todos}
+        handleEditTodo={handleEditTodo}
+        handleDeleteTodo={handleDeleteTodo}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 export default function TodoCard(props: {
   todo: string;
   handleEditTodo: (oldTodo: string, newTodo: string) => void;
   handleDeleteTodo: (todo: string) => void;
 }) {
   const { todo  , handleDeleteTodo , handleEditTodo } = props;
+  const [isEditing, setIsEditing] = useState<boolean>(false);
   return (
     <>
       <li className="todoItem">
-        {todo}
+        {isEditing ? (
+          <input
+          style={{ width: "100%", padding: "10px", borderRadius: "5px", border: "1px solid #ccc" }}
+            type="text"
+            value={todo}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setIsEditing(false);
+              }
+            }}
+            onChange={(e) => handleEditTodo(todo, e.target.value)}
+          />
+        ) : (
+          <>{todo}</>
+        )}
         <div className="actionsContainer">
-          <i onClick={() =>  handleEditTodo(todo ,todo) } className="fa-solid fa-pen-to-square"></i>
+          <i onClick={() =>  setIsEditing(!isEditing) } className="fa-solid fa-pen-to-square"></i>
           <i className="fa-regular fa-trash-can" onClick={() => handleDeleteTodo(todo)}></i>
         </div>
       </li>
